a8a97b00e277de8955bfa5b2c5cbb833ea5fa4b3
Mhbib34 <mhabib34official@gmail.com>
2025-02-04 16:18:47 +0700

    menghapus props onclick pada component Button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,9 +1,8 @@
-function Button({ type, className, text, onClick }) {
+function Button({ type, className, text }) {
   return (
     <button
       type={type}
       className={`p-1 px-2 border border-[#9bf272] cursor-pointer rounded-lg ${className} `}
-      onClick={onClick}
     >
       {text}
     </button>

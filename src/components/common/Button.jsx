c6e75eeccd6812ed1c3a4e668761825f46abf706
Mhbib34 <mhabib34official@gmail.com>
2025-02-04 16:16:51 +0700

    Membuat responsive Navbar

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,8 +1,9 @@
-function Button({ type, className, text }) {
+function Button({ type, className, text, onClick }) {
   return (
     <button
       type={type}
       className={`p-1 px-2 border border-[#9bf272] cursor-pointer rounded-lg ${className} `}
+      onClick={onClick}
     >
       {text}
     </button>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,15 +2,34 @@ import Button from "../common/Button";
 import Input from "../common/Input";
 
 function Navbar() {
+  function handleInputSearch(e) {
+    e.preventDefault();
+    localStorage.setItem("searchInput", e.target.searchInput.value);
+    e.target.searchInput.value = "";
+  }
   return (
     <div className="bg-[#2b2b2b] w-[80%] flex items-center justify-center m-auto my-2 p-2 rounded-full">
       <div className="flex justify-between w-full items-center">
         <div className="w-full h-full flex px-2 mx-4 py-1 items-center rounded-md  ">
           <i className="bx bx-search-alt text-white text-2xl" undefined></i>
-          <Input type="text" name="searchInput" placeholder="Cari di sini..." />
-          <i class="bx bxs-cart text-white text-2xl" undefined></i>
+          <form className="w-full" onSubmit={handleInputSearch}>
+            <Input
+              type="text"
+              name="searchInput"
+              placeholder="Cari barang..."
+            />
+          </form>
+          <div className="hidden md:flex">
+            <i
+              class="bx bxs-cart text-white text-2xl w-full h-full"
+              undefined
+            ></i>
+          </div>
         </div>
-        <div className="flex gap-2 justify-center items-center border-l-2 border-white px-4 ">
+        <div className="md:hidden flex items-center">
+          <i className="bx bx-menu text-white text-2xl px-4"></i>
+        </div>
+        <div className="hidden md:flex gap-2 justify-center items-center md:border-l-2 md:border-white px-4  ">
           <Button
             type="submit"
             className={

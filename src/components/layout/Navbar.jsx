e3733f095ce23b278e23407100fc354cc22ee386
Mhbib34 <mhabib34official@gmail.com>
2025-02-11 23:31:40 +0700

    Menambahkan logo pada navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,16 @@
 import HamburgerMenu from "../common/HamburgerMenu";
 import Form from "../Fragments/Form";
 import IndexButton from "../Fragments/IndexButton";
+import Logo from "../../assets/img/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="bg-[#2b2b2b] flex items-center justify-center m-auto sticky top-2 z-10 p-2 rounded-lg">
       <div className="flex justify-between w-full items-center">
+        <Link to="/">
+          <img src={Logo} alt="" className="h-12 w-32" />
+        </Link>
         <Form />
         <HamburgerMenu />
         <IndexButton />
